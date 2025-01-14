2daea2944476f1e0fd3396af60b77a7f642f9c43
tc2323 <tc2323@ic.ac.uk>
2025-01-14 18:16:56 +0000

    Added more queries and tests for them

--- src/main/java/ic/doc/QueryProcessor.java
+++ src/main/java/ic/doc/QueryProcessor.java
@@ -17,6 +17,50 @@ public class QueryProcessor {
                            "his works of hard science fiction and popular science. \n");
             results.append(System.lineSeparator());
         }
+
+        if (query.toLowerCase().contains("clausewitz")) {
+            results.append("Carl Philipp Gottfried von Clausewitz (1 July 1780 - 16 November 1831)\n" +
+                           "was a Prussian general and military theorist. His most notable work,\n" +
+                           "Vom Kriege (On War), remains unfinished and is considered a seminal\n" +
+                           "treatise on military strategy and science.\n");
+            results.append(System.lineSeparator());
+        }
+
+        if (query.toLowerCase().contains("meiji")) {
+            results.append("Mutsuhito (3 November 1852 - 30 July 1912), posthumously honored as\n" +
+                           "Emperor Meiji, was the 122nd Emperor of Japan (1867-1912), and is\n" +
+                           "known for the Meiji Restoration of 1868, which industrialised Japan,\n" +
+                           "turning the isolationist island nation into a great power.\n");
+            results.append(System.lineSeparator());
+        }
+
+        if (query.toLowerCase().contains("rowling")) {
+            results.append("J. K. Rowling (31 July 1965 - present) is a British author and\n" +
+                           "philanthropist. She is best known for writing the Harry Potter\n" +
+                           "fantasy series, published from 1997 to 2007. \n");
+            results.append(System.lineSeparator());
+        }
+
+        if (query.toLowerCase().contains("mj")) {
+            results.append("Michael Joseph Jackson (August 29, 1958 – June 25, 2009) was an American\n" +
+                           "singer, songwriter, dancer, and philanthropist. Dubbed the 'King of Pop',\n" +
+                           "he is regarded as one of the most significant cultural figures ever. \n");
+            results.append(System.lineSeparator());
+        }
+
+        if (query.toLowerCase().contains("darwin")) {
+            results.append("Charles Robert Darwin (12 February 1809 - 19 April 1882) was an\n" +
+                           "English naturalist, geologist, and biologist, widely known for his\n" +
+                           "contributions to evolutionary biology.\n");
+            results.append(System.lineSeparator());
+        }
+        if (query.toLowerCase().contains("einstein")) {
+            results.append("Albert Einstein (14 March 1879 - 18 April 1955) was a German-born\n" +
+                           "theoretical physicist who is best known for developing the theory of\n" +
+                           "relativity. Einstein also made important contributions to quantum mechanics.\n"
+                           );
+            results.append(System.lineSeparator());
+        } 
         return results.toString();
     }
 }
--- src/test/java/ic/doc/QueryProcessorTest.java
+++ src/test/java/ic/doc/QueryProcessorTest.java
@@ -26,6 +26,36 @@ public class QueryProcessorTest {
     }
 
     @Test
+    public void knowsAboutClausewitz() throws Exception {
+        assertThat(queryProcessor.process("Clausewitz"), containsString("On War"));
+    }
+
+    @Test
+    public void knowsAboutMeiji() throws Exception {
+        assertThat(queryProcessor.process("Meiji"), containsString("Emperor"));
+    }
+
+    @Test
+    public void knowsAboutRowling() throws Exception {
+        assertThat(queryProcessor.process("Rowling"), containsString("Harry Potter"));
+    }
+
+    @Test
+    public void knowsAboutMJ() throws Exception {
+        assertThat(queryProcessor.process("MJ"), containsString("King of Pop"));
+    }
+
+    @Test
+    public void knowsAboutDarwin() throws Exception {
+        assertThat(queryProcessor.process("Darwin"), containsString("evolutionary biology"));
+    }
+
+    @Test
+    public void knowsAboutEinstein() throws Exception {
+        assertThat(queryProcessor.process("Einstein"), containsString("quantum mechanics"));
+    }
+
+    @Test
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
