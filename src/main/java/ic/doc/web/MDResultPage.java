d2a313726b59cf0c63b2f86469ff6b8aa9e2daad
Wei Ng <wjn23@willow15.doc.ic.ac.uk>
2025-01-16 12:13:40 +0000

    implemented new MDResultPage, unable to check if it runs

--- src/main/java/ic/doc/web/MDResultPage.java
+++ src/main/java/ic/doc/web/MDResultPage.java
@@ -21,22 +21,44 @@ public class MDResultPage implements Page {
 
     @Override
     public void writeTo(HttpServletResponse resp) throws IOException {
-        // Set response headers to enable file download
-        resp.setContentType("text/markdown");
+        // Set the content type to Plain text
+        resp.setContentType("text/plain");
+
+        // Set the Content-Disposition header to trigger download
         resp.setHeader("Content-Disposition", "attachment; filename=\"result.md\"");
 
-        // Create Markdown content
-        StringBuilder markdownContent = new StringBuilder();
-        markdownContent.append("# ").append(query).append("\n\n");
-        if (answer == null || answer.isEmpty()) {
-            markdownContent.append("Sorry, we didn't understand the query.\n");
-        } else {
-            markdownContent.append(answer).append("\n");
+        // Create a temporary file
+        File tempFile = File.createTempFile("result", ".md");
+        tempFile.deleteOnExit();
+
+        // Write the query result to the file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+            if (answer == null || answer.isEmpty()) {
+                writer.write("# " + query + "\n");
+                writer.write("Sorry, we didn't understand the query.");
+            } else {
+                writer.write("# " + query + "\n");
+                writer.write(answer);
+            }
         }
 
-        // Write Markdown content to the response output stream
-        try (OutputStream os = resp.getOutputStream()) {
-            os.write(markdownContent.toString().getBytes());
+        // Serve the file data to the user
+        try (InputStream in = new FileInputStream(tempFile);
+             OutputStream out = resp.getOutputStream()) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.flush(); // Ensure all data is sent to the client
         }
     }
+
+
+        // Write Markdown content to the response output stream
+        // try (OutputStream os = resp.getOutputStream()) {
+        //     os.write(markdownContent.toString().getBytes());
+        // }
+    
 }
